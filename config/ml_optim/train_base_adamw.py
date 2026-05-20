b747dd20480f24cb934359562a3dbc742e843c86
davidfraenkle <david.m.fraenkle@gmail.com>
2026-05-20 14:41:15 +0200

    added new config

--- config/ml_optim/train_base_adamw.py
+++ config/ml_optim/train_base_adamw.py
@@ -33,8 +33,8 @@ n_embd = 384
 # 12 batch size * 1024 block size * 5 gradaccum * 8 GPUs = 491,520
 batch_size = 32
 block_size = 1024
-gradient_accumulation_steps = 8
-gpu_count = 4
+gradient_accumulation_steps = 32
+gpu_count = 1
 
 tokens_iter = batch_size * block_size * gradient_accumulation_steps * gpu_count
 tokens_expert = tokens_iter * top_k / n_exp
