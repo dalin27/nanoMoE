6907bb97573315e8bbd1ad6b1f6ce4305bf6cb53
davidfraenkle <david.m.fraenkle@gmail.com>
2026-05-21 10:20:29 +0200

    added new config

--- config/ml_optim/train_base_adamw.py
+++ config/ml_optim/train_base_adamw.py
@@ -59,6 +59,6 @@ grad_clip = 1.0 # clip gradients at this value, or disable if == 0.0
 
 # learning rate decay settings
 decay_lr = True # whether to decay the learning rate
-warmup_iters = 150 # how many steps to warm up for
+warmup_iters = 300 # how many steps to warm up for
 learning_rate = 6e-4 # max learning rate
 min_lr = 6e-5 # minimum learning rate, should be ~= learning_rate/10 per Chinchilla
