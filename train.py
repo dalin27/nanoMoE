c21445bf0e33b85f5436278f1c3fa9e919346299
davidfraenkle <david.m.fraenkle@gmail.com>
2026-06-09 11:50:51 +0200

    fixes

--- train.py
+++ train.py
@@ -663,7 +663,7 @@ while True:
                 ema_grad = alpha * total_norm + (1 - alpha) * ema_grad
                 ema_dropped = alpha * total_dropped + (1 - alpha) * ema_dropped
 
-            if master_process and wandb_log and iter_num % wandb_interval:
+            if master_process and wandb_log and iter_num % wandb_interval == 0:
                 wandb.log({
                     "EMA/CV": ema_cv,
                     "EMA/Loss": ema_loss,
