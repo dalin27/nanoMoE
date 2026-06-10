305b412d14ef8d7a215e2afb07b97ee132aabdcd
davidfraenkle <david.m.fraenkle@gmail.com>
2026-06-10 13:28:21 +0200

    fixes

--- config/ml_optim/env2/adamw.py
+++ config/ml_optim/env2/adamw.py
@@ -66,5 +66,5 @@ learning_rate = 6e-4            # Remember to scale this down if passing Lion!
 min_lr = 6e-5
 
 # Homogeneous Data Shock Parameters
-step_shock_start = 1500
-step_recovery_start = 2000
+step_shock_start = 2500
+step_recovery_start = 3000
